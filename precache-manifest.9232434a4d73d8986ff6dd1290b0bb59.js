e1bafd6084b5a861a0e36d7b906227af3670d306
Ryan Fabela <01somebody@gmail.com>
2021-12-20 02:54:43 -0600

    Updates

---
+++ precache-manifest.9232434a4d73d8986ff6dd1290b0bb59.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2362e4219e20578f7b1a48a3ed1727",
+    "url": "/ryanfabelacom/index.html"
+  },
+  {
+    "revision": "b6068e79d736020005f1",
+    "url": "/ryanfabelacom/static/css/2.2bc85907.chunk.css"
+  },
+  {
+    "revision": "1ae441dad8782f24fb82",
+    "url": "/ryanfabelacom/static/css/main.2fcbe09b.chunk.css"
+  },
+  {
+    "revision": "b6068e79d736020005f1",
+    "url": "/ryanfabelacom/static/js/2.8f52dd0d.chunk.js"
+  },
+  {
+    "revision": "6cfffc21677fea4ccac09045cec2b890",
+    "url": "/ryanfabelacom/static/js/2.8f52dd0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae441dad8782f24fb82",
+    "url": "/ryanfabelacom/static/js/main.d021e5d6.chunk.js"
+  },
+  {
+    "revision": "7f249906d508be4a685d",
+    "url": "/ryanfabelacom/static/js/runtime-main.5bf4534e.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ryanfabelacom/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ryanfabelacom/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ryanfabelacom/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ryanfabelacom/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2096cb3bde5621966715eae4f8e5a05f",
+    "url": "/ryanfabelacom/static/media/slide-2.2096cb3b.jpg"
+  },
+  {
+    "revision": "ffa58932303686f05851948fb73cd602",
+    "url": "/ryanfabelacom/static/media/slide-22.ffa58932.jpg"
+  }
+]);
